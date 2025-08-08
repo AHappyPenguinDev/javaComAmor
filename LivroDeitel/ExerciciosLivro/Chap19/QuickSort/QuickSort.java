07b5845c7e43588f12873493df174b9bc932fc44
hamstermech <kfriesencold@gmail.com>
2025-08-08 18:15:25 +0000

    Chap 19 done for now, missing 2 exercises

--- LivroDeitel/ExerciciosLivro/Chap19/QuickSort/QuickSort.java
+++ LivroDeitel/ExerciciosLivro/Chap19/QuickSort/QuickSort.java
@@ -2,26 +2,42 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-  public static void quickSort(int[] array) {
-    quickSort(array, 0, array.length - 1);
-  }
+  public static void quickSortHelper(int[] array, int start, int end) {
+    if (start < end) { // base case if subarray has more than one element
+      int partElement = quickSort(array, start, end); //
 
-  private static void quickSort(int[] array, int start, int end) {
-    for (int i = end; i > 0; i--) {
-      if (array[start] <= array[end]) {
-        int temp = array[start];
-        array[start] = array[end];
-        array[end] = temp;
-      }
+      quickSortHelper(array, start, partElement - 1);
+      quickSortHelper(array, partElement + 1, end);
     }
   }
 
-  public static void main(String[] args) {
-    int[] array = { 85, 3, 10, 5, 653, 23, 48 };
-    int[] preSort = array.clone();
+  private static int quickSort(int[] array, int start, int end) { // quickSort returns the
+    int pivot = array[end]; // choose last element as the pivot
+    int i = start - 1; // i represented the boundary of elements less than or equal to the pivot
 
-    quickSort(array);
-    System.out.printf("Array before sort: %s%n", Arrays.toString(preSort));
+    for (int j = start; j < end; j++) {
+      if (array[j] <= pivot) {
+        i++;
+
+        swap(array, i, j);
+      }
+    }
+
+    swap(array, i + 1, end);
+    return i + 1;
+  }
+
+  private static void swap(int[] array, int left, int right) { // swap two elements
+    int temp = array[left];
+    array[left] = array[right];
+    array[right] = temp;
+  }
+
+  public static void main(String[] args) {
+    int[] array = { 37, 2, 6, 4, 89, 8, 10, 12, 68, 45 }; // create array
+
+    System.out.printf("Array before sort: %s%n", Arrays.toString(array));
+    quickSortHelper(array, 0, array.length - 1); // call quickSort helper to sort array
     System.out.printf("Array after sort: %s%n", Arrays.toString(array));
   }
 }
